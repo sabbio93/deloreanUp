cddd952755d98b25277ea443e180ee4c3c083f32
Alessandro Staffolani <alestam93@gmail.com>
2019-04-20 19:00:49 +0200

    Added a unique id for the node

--- Backend/src/Plugins/DocNodesFinder/staticCode.js
+++ Backend/src/Plugins/DocNodesFinder/staticCode.js
@@ -1,8 +1,8 @@
 import DocNode from '../../model/DocNode'
 
 const docNodes = [
-  new DocNode('127.0.0.1', 5555),
-  new DocNode('192.168.0.13', 5555)
+  new DocNode('1', '127.0.0.1', 5555),
+  new DocNode('2', '192.168.0.13', 5555)
 ]
 
 function getAllNodes () {
--- Backend/src/model/DocNode.js
+++ Backend/src/model/DocNode.js
@@ -1,10 +1,12 @@
 class DocNode {
   /**
-     * Create a Doc Node reference
-     * @param ip {String}
-     * @param port {int}
-     */
-  constructor (ip, port) {
+   * Create a Doc Node reference
+   * @param {string} id - unique identifier for the node
+   * @param {string} ip - ip of the node
+   * @param {int} port - port of the node
+   */
+  constructor (id, ip, port) {
+    this.id = id
     this.ip = ip
     this.port = port
   }
