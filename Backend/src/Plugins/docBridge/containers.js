89821ba4c45ca84e01eb0ee5615b6cd59ad7e0d1
Alessandro Staffolani <alestam93@gmail.com>
2019-04-24 13:26:14 +0200

    Changed name of the route to list node containers

--- Backend/src/Plugins/docBridge/containers.js
+++ Backend/src/Plugins/docBridge/containers.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
 /**
- * Method to call inside a node the route `containers/list`
+ * Method to call inside a node the route `/api/{version}/containers` that return the list of active containers
  * @param {DocNode} node - {@link DocNode} object
  * @returns {AxiosPromise} - promise of the get response, resolved with the list and reject in case of errors
  */
 const getContainersList = (node) => {
-  const path = node.getUrl() + '/api/v' + node.apiVersion + '/containers/list'
+  const path = node.getUrl() + '/api/v' + node.apiVersion + '/containers'
   return axios.get(path)
 }
 
