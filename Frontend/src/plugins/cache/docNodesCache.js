2d624bc7f0f9952a0352be879a40481f261e1446
Eugenio Cavina <ecavina@imolinfo.it>
2019-05-19 17:25:38 +0200

    Added node cache addNode and getNodes

--- Frontend/src/plugins/cache/docNodesCache.js
+++ Frontend/src/plugins/cache/docNodesCache.js
@@ -3,7 +3,7 @@
 const IDB = window.indexedDB
 const DB_NAME = 'DeloreanStorage'
 const STORE_NAME = 'nodeOS'
-const OS_KEY = 'nodeId'
+const OS_KEY = 'id'
 const VERSION = 1
 
 /**
@@ -42,14 +42,96 @@ const openDatabase = () => {
 }
 
 const getDatabaseConnection: IndexedDatabaseConnection = (openedDB) => {
-  let db: IndexedDatabaseConnection = {}
-  
-  db.result = openedDB.result
-  db.tx = db.result.transaction(STORE_NAME, 'readwrite')
-  db.store = db.tx.objectStore(STORE_NAME)
+  let connection: IndexedDatabaseConnection = {}
 
-  return db
+  connection.result = openedDB.result
+  connection.tx = connection.result.transaction(STORE_NAME, 'readwrite')
+  connection.store = connection.tx.objectStore(STORE_NAME)
+
+  return connection
 }
+
+export function clearCache (): Promise<ResponseObject> {
+  return new Promise((resolve, reject) => {
+    let db = openDatabase()
+    let response: ResponseObject = {}
+
+    db.onsuccess = () => {
+      let connection = getDatabaseConnection(db)
+      let result = connection.store.clear()
+      result.onsuccess = (event) => {
+        response.success = true
+        response.data = event.type
+        resolve(response)
+      }
+      result.onerror = (event) => {
+        response.success = false
+        response.data = event.type
+        reject(response)
+      }
+    }
+  })
+}
+
+export function addNode (node: Object): Promise<ResponseObject> {
+  return new Promise((resolve, reject) => {
+    let db = openDatabase()
+    let response: ResponseObject = {}
+
+    db.onsuccess = () => {
+      let connection = getDatabaseConnection(db)
+      let result = connection.store.add(node)
+      result.onsuccess = (event) => {
+        response.success = true
+        response.data = event.type
+        resolve(response)
+      }
+      result.onerror = (event) => {
+        response.success = false
+        response.data = event.type
+        reject(response)
+      }
+    }
+  })
+}
+
+// function addContainer (nodeId: string, container: Object): Promise<ResponseObject> {
+// return
+// }
+
+export function getNodes (): Promise<ResponseObject> {
+  return new Promise((resolve, reject) => {
+    let db = openDatabase()
+    let response: ResponseObject = {}
+
+    db.onsuccess = () => {
+      let connection = getDatabaseConnection(db)
+      let result = connection.store.getAll()
+      result.onsuccess = (event) => {
+        response.data = event.target.result
+        response.success = true
+        resolve(response)
+      }
+      result.onerror = (event) => {
+        response.data = event.type
+        response.success = false
+        reject(response)
+      }
+    }
+  })
+}
+
+// function getNodeContainers (nodeId: string): Promise<ResponseObject> {
+//   return
+// }
+
+// function getNodeContainerById (nodeId: string, containerId: string): Promise<ResponseObject> {
+//   return
+// }
+
+// function getNodeContainerMounts (nodeId: string, containerId: string): Promise<ResponseObject> {
+//   return
+// }
 
 export function deleteCache (): Promise<ResponseObject> {
   let responseObject: ResponseObject = { }
@@ -68,40 +150,3 @@ export function deleteCache (): Promise<ResponseObject> {
     }
   })
 }
-
-export function clearCache (): Promise<ResponseObject> {
-  return new Promise((resolve, reject) => {
-    let db = openDatabase()
-
-    db.onsuccess = () => {
-      let connection = getDatabaseConnection(db)
-      let result = connection.store.clear()
-      result.onsuccess = (event) => resolve(event)
-      result.onerror = (event) => reject(event)
-    }
-  })
-}
-
-// function addNode (): Promise<ResponseObject> {
-//   return
-// }
-
-// function addContainer (nodeId: string): Promise<ResponseObject> {
-//   return
-// }
-
-// function getNodes (): Promise<ResponseObject> {
-//   return
-// }
-
-// function getNodeContainers (nodeId: string): Promise<ResponseObject> {
-//   return
-// }
-
-// function getNodeContainerById (nodeId: string, containerId: string): Promise<ResponseObject> {
-//   return
-// }
-
-// function getNodeContainerMounts (nodeId: string, containerId: string): Promise<ResponseObject> {
-//   return
-// }
