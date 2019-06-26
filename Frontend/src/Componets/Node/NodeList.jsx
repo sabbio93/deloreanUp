8ac5b6b4e280c0f0064886c6eaed4a22f86827c4
Alessandro Staffolani <alestam93@gmail.com>
2019-06-27 00:49:35 +0200

    NodeList refactoring

--- Frontend/src/Componets/Node/NodeList.jsx
+++ Frontend/src/Componets/Node/NodeList.jsx
@@ -1,6 +1,7 @@
 // @flow
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 import Node from './Node'
+import NodeCopy from './NodeCopy'
 import List, {
   ListGroup,
   ListGroupSubheader, ListItem, ListItemText, ListItemGraphic
@@ -14,51 +15,96 @@ type Props = {
   handleBackupListChange: Function
 }
 
-function NodeList (props: Props) {
-  const [ selectedIndex, setSelectedIndex ] = useState(null)
-  const { nodes, toggleDialogContainer, handleBackupListChange } = props
+type State = {
+  selectedIndex: number | null
+}
 
-  function handleNodeClick (index) {
-    if (selectedIndex === index) {
-      setSelectedIndex(null)
-    } else {
-      setSelectedIndex(index)
+class NodeList extends Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedIndex: null
     }
   }
 
-  // Create the node list elements
-  const nodesList = nodes.map((node, index) => (
-    <Node
-      key={index}
-      node={node}
-      active={selectedIndex === index}
-      index={index}
-      onClick={handleNodeClick}
-      toggleDialogContainer={toggleDialogContainer}
-      handleBackupListChange={handleBackupListChange}
-    />
-  ))
+  handleNodeClick = (index) => {
+    const { selectedIndex } = this.state
+    if (selectedIndex === index) {
+      this.setState({ selectedIndex: null })
+    } else {
+      this.setState({ selectedIndex: index })
+    }
+  }
 
-  return (
-    <div className='node-list-wrapper'>
-      <ListGroup>
-        <ListGroupSubheader tag='div'><Headline5 tag='h2'>Active Doc Nodes</Headline5></ListGroupSubheader>
-        <List nonInteractive>
-          <ListItem>
-            <ListItemGraphic graphic={<MaterialIcon icon='' />} />
-            <ListItemText primaryText={<div className='node-list-header'>
-              <span className='node-id'>ID</span>
-              <span className='node-host'>HOST</span>
-              <span className='node-api-version'>API Version</span>
-            </div>} />
-          </ListItem>
-        </List>
-        <List>
-          {nodesList}
-        </List>
-      </ListGroup>
-    </div>
-  )
+  render() {
+    const { selectedIndex } = this.state
+    const { nodes, toggleDialogContainer, handleBackupListChange } = this.props
+
+    // Create the node list elements
+    const nodesList = nodes.map((node, index) => (
+      <Node
+        key={index}
+        node={node}
+        active={selectedIndex === index}
+        index={index}
+        onClick={this.handleNodeClick}
+        toggleDialogContainer={toggleDialogContainer}
+        handleBackupListChange={handleBackupListChange}
+      />
+    ))
+
+    // Create the node list elements
+    const nodesTable = nodes.map((node, index) => (
+      <NodeCopy
+        key={index}
+        node={node}
+        active={selectedIndex === index}
+        index={index}
+        onClick={this.handleNodeClick}
+        toggleDialogContainer={toggleDialogContainer}
+        handleBackupListChange={handleBackupListChange}
+      />
+    ))
+
+    return (
+      <div className='node-list-wrapper'>
+        <Headline5 tag='h2'>Active Doc Nodes</Headline5>
+        <div className='delorean-table-wrapper'>
+          <table className='delorean-table hoverable main-table'>
+            <thead>
+            <tr>
+              <th/>
+              <th>ID</th>
+              <th>HOST</th>
+              <th>API Version</th>
+              <th/>
+            </tr>
+            </thead>
+            <tbody>
+            {nodesTable}
+            </tbody>
+          </table>
+        </div>
+
+        <ListGroup>
+          <ListGroupSubheader tag='div'><Headline5 tag='h2'>Active Doc Nodes</Headline5></ListGroupSubheader>
+          <List nonInteractive>
+            <ListItem>
+              <ListItemGraphic graphic={<MaterialIcon icon='' />} />
+              <ListItemText primaryText={<div className='node-list-header'>
+                <span className='node-id'>ID</span>
+                <span className='node-host'>HOST</span>
+                <span className='node-api-version'>API Version</span>
+              </div>} />
+            </ListItem>
+          </List>
+          <List>
+            {nodesList}
+          </List>
+        </ListGroup>
+      </div>
+    )
+  }
 }
 
 NodeList.defaultProps = {
